80b04a3f782963b2fef601bd26726b9bf6dd08be
Nitin Singh <borntocode.ea@gmail.com>
2024-11-21 07:48:15 +0530

    updated backend url in frontend

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -42,7 +42,7 @@ export const SendMoney = () => {
                     />
                     </div>
                     <button onClick={() => {
-                        axios.post("https://paytm-vercel.vercel.app/api/v1/account/transfer", {
+                        axios.post("https://paytm-nu-pink.vercel.app//api/v1/account/transfer", {
                             to: id,
                             amount
                         }, {
