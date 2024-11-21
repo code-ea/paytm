7b0be48951925eda4ce23cf1a4b11a0292dd9559
Nitin Singh <borntocode.ea@gmail.com>
2024-11-21 08:42:10 +0530

    removed app.use(cors({ origin: 'https://paytm-t8rv.vercel.app' }));

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: 'https://paytm-t8rv.vercel.app' }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", mainRouter);
