49fdb2e8c5743534a92dbec1a0d3819fdc2a52fa
Nitin Singh <borntocode.ea@gmail.com>
2024-11-24 23:02:39 +0530

    Corrected typos

--- frontend/src/pages/UpdateProfile.jsx
+++ frontend/src/pages/UpdateProfile.jsx
@@ -18,8 +18,8 @@ export const UpdateProfile = () => {
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-        <Heading label={"Sign up"} />
-        <SubHeading label={"Enter your infromation to create an account"} />
+        <Heading label={"Update Details"} />
+        <SubHeading label={"Enter your new details to update your account"} />
         <InputBox onchange={e => {
           setFirstName(e.target.value);
         }} placeholder="John" label={"First Name"} />
